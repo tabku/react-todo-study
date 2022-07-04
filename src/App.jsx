642604d4ab0e191d51dea4537d813a34cb95a378
tabku <41511147+tabku@users.noreply.github.com>
2022-07-05 08:49:29 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div>hoge</div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>ああああ</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
